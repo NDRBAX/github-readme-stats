954edb4f0c27160e4a21d41b512cf867bf077369
NDRBAX <96124311+NDRBAX@users.noreply.github.com>
2022-08-21 19:25:10 +0200

    Update index.js

--- themes/index.js
+++ themes/index.js
@@ -166,7 +166,7 @@ const themes = {
     title_color: "7fff00",
     icon_color: "00AEFF",
     text_color: "fff",
-    bg_color: "%23ff",
+    bg_color: "FFFFFFFF",
   },
   nord: {
     title_color: "81a1c1",
